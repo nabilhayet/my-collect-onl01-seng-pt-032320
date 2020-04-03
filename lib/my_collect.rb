c455483aace7bfb7eb2a1e58118ea97288787513
Nabil Hayet <nabil.bracu@gmail.com>
2020-04-03 04:54:19 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+def my_collect(collection)
+  i = 0
+  collect = []
+  while i < collection.length
+    collect << yield(collection[i])
+    i += 1
+  end
+  collect
+end
 
-
+##my_collect(collection) do |lang|
+  ##lang.upcase
+##end 
